0e842ef2159a7a2245283d58b771ad2298cc44df
王金华 <tony.wang@vipshop.com>
2014-11-27 14:06:39 +0800

    test commit

--- athena-main/src/main/java/org.txxf.athena/Main.java
+++ athena-main/src/main/java/org.txxf.athena/Main.java
@@ -6,7 +6,7 @@ package org.txxf.athena;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("java");
+        System.out.println("java hello");
     }
 
 
