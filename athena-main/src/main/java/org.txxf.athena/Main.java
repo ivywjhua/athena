82a70af479e5154c29b530d158a66be690972846
王金华 <tony.wang@vipshop.com>
2014-11-27 14:03:38 +0800

    init

---
+++ athena-main/src/main/java/org.txxf.athena/Main.java
@@ -0,0 +1,13 @@
+package org.txxf.athena;
+
+/**
+ * Created by tony.wang on 2014/11/27.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("java");
+    }
+
+
+}
